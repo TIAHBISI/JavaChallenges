9547ce001d8121d61e846a1e363d0aa3ac83aa98
Byers <M2003744@middlesbro.ac.uk>
2022-09-21 16:21:47 +0100

    Five Times Table

--- src/main/java/WhileLoops/WhileLoop5TimesTable.java
+++ src/main/java/WhileLoops/WhileLoop5TimesTable.java
@@ -5,19 +5,20 @@ package WhileLoops;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author m2003744
  */
 public class WhileLoop5TimesTable {
-    
+
     public static void main(String[] args) {
-        
+        int n = 5;
+        int range = 15;
+        System.out.println("Five Times Table with Range 1 to 15");
+        int i = 1;
+        while (i <= range) {
+            System.out.println(n + " * " + i + " = " + n * i);
+            i++;
+        }
     }
-    
-    public void test(){
-        System.out.println("Hello World");
-    }
-    
 }
